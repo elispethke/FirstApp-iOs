27378e1956cee1ab2e33b6d67108b26dbec823a9
Elis Pethke <elispethke@gmail.com>
2023-04-26 00:36:05 +0200

    Initial Commit

---
+++ AppTempo/AppTempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTempoApp.swift
+//  AppTempo
+//
+//  Created by Elisangela Pethke on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AppTempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
